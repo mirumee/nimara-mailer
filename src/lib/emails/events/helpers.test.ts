aad5b013e9b721176560b0ee437a976af9312162
Piotr Grundas <piotrgrundas@gmail.com>
2024-10-17 13:09:05 +0200

    [MS-780] feat: Add local smtp tooling (#23)
    * [MS-780] feat: Add local smtp tooling
    
    * [MS-780] feat: Improve payload serialization
    
    * [MS-780] feat: Update readme

---
+++ src/lib/emails/events/helpers.test.ts
@@ -0,0 +1,92 @@
+import { describe, expect, it } from "vitest";
+import { z } from "zod";
+
+import { parsePayload, serializePayload, SUPPORTED_EVENTS } from "./helpers";
+
+describe("helpers", () => {
+  describe("serializePayload", () => {
+    it("should serialize payload correctly when valid data is provided", () => {
+      // given
+      const mockData = { key: "value" };
+      const mockEvent = SUPPORTED_EVENTS[0];
+
+      // when
+      const serialized = serializePayload({
+        data: mockData,
+        event: mockEvent,
+      });
+
+      // then
+      expect(serialized).toEqual({
+        format: expect.any(String),
+        payload: {
+          event: mockEvent,
+          data: mockData,
+        },
+      });
+    });
+
+    it("should throw an error when invalid event is provided", () => {
+      // given
+      const mockData = { key: "value" };
+      const invalidEvent = "invalid_event";
+
+      // when & then
+      expect(() => {
+        serializePayload({
+          data: mockData,
+          event: invalidEvent as any,
+        });
+      }).toThrow(z.ZodError);
+    });
+
+    it("should throw an error when invalid data format is provided", () => {
+      // given
+      const invalidData = null;
+      const mockEvent = "order_created";
+
+      // when / then
+      expect(() => {
+        serializePayload({
+          data: invalidData as any,
+          event: mockEvent,
+        });
+      }).toThrow(z.ZodError);
+    });
+  });
+
+  describe("parsePayload", () => {
+    it("should successfully parse valid payload data", () => {
+      // given
+      const validData = {
+        format: "json",
+        payload: {
+          event: "order_created",
+          data: { key: "value" },
+        },
+      };
+
+      // when
+      const parsedData = parsePayload(validData);
+
+      // then
+      expect(parsedData).toEqual(validData);
+    });
+
+    it("should throw an error when wrong data is passed", () => {
+      // given
+      const invalidData = {
+        format: null,
+        payload: {
+          data: { key: "value" },
+          event: "wrong",
+        },
+      };
+
+      // when / then
+      expect(() => {
+        parsePayload(invalidData);
+      }).toThrow(z.ZodError);
+    });
+  });
+});
--- src/lib/emails/events/helpers.ts
+++ src/lib/emails/events/helpers.ts
@@ -1,21 +1,40 @@
 import { type FastifyRequest } from "fastify";
+import { z } from "zod";
 
+import { EVENT_HANDLERS } from "@/api/rest/saleor/webhooks";
 import { CONFIG } from "@/config";
 import { type WebhookEventTypeAsyncEnum } from "@/graphql/schema";
 import { getJSONFormatHeader } from "@/lib/saleor/apps/utils";
+
+export const SUPPORTED_EVENTS = EVENT_HANDLERS.map(({ event }) =>
+  event.toLowerCase()
+) as any as z.EnumValues<Lowercase<WebhookEventTypeAsyncEnum>>;
+
+export type Event = (typeof SUPPORTED_EVENTS)[number];
+
+export const payloadSchema = z.object({
+  format: z.string(),
+  payload: z.object({
+    data: z.object({}).passthrough(),
+    event: z.enum(SUPPORTED_EVENTS),
+  }),
+});
 
 export const serializePayload = ({
   data,
   event,
 }: {
   data: FastifyRequest["body"];
-  event: Lowercase<WebhookEventTypeAsyncEnum>;
-}) => ({
-  format: getJSONFormatHeader({ name: CONFIG.NAME }),
-  payload: {
-    event,
-    data,
-  },
-});
+  event: Event;
+}) =>
+  payloadSchema.parse({
+    format: getJSONFormatHeader({ name: CONFIG.NAME }),
+    payload: {
+      event,
+      data,
+    },
+  });
+
+export const parsePayload = (data: unknown) => payloadSchema.parse(data);
 
 export type SerializedPayload = ReturnType<typeof serializePayload>;
