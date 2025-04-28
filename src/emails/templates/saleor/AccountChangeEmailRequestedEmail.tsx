71cf92b3a8b71c3407068434a00f2473e8c242a2
soniaklimas <sonia.klimas@gmail.com>
2025-04-28 08:31:46 +0200

    Adjust email change request email message

--- src/emails/templates/saleor/AccountChangeEmailRequestedEmail.tsx
+++ src/emails/templates/saleor/AccountChangeEmailRequestedEmail.tsx
@@ -20,9 +20,18 @@ const AccountChangeEmailRequestedEmail = ({
         <>
           <Header>Hi {data.user?.firstName}!</Header>
           <Text>
-            Your email address change has been requested. If you want to confirm
-            changing the email address to <strong>{data.newEmail}</strong>,
-            please follow the link:
+            {
+              "Your email address change has been requested. If you want to confirm changing the email address"
+            }
+            {data.newEmail ? (
+              <>
+                {" to "}
+                <strong>{data.newEmail}</strong>
+                {", please follow the link:"}
+              </>
+            ) : (
+              ", please follow the link:"
+            )}
             <br />
             <Link
               href={data.redirectUrl ?? "#"}
